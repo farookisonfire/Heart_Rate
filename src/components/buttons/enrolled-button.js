618d85020c31664a92934bbc0fcdb66cc00a7ecf
Farook Khan <farook.khan@gmail.com>
2016-11-01 17:18:52 -0700

    Issue #11 - User Can Toggle Chart Views

--- src/components/buttons/enrolled-button.js
+++ src/components/buttons/enrolled-button.js
@@ -1,11 +1,28 @@
 const React = require('react');
+import FlatButton from 'material-ui/FlatButton';
 import IconButton from 'material-ui/IconButton';
+import Assignment from 'material-ui/svg-icons/action/assignment';
 import CheckCircle from 'material-ui/svg-icons/action/check-circle';
-const { showEnrolled } = require('../../actions')
-const styles = require('./styles')
-const { connect } = require('react-redux')
+import Equalizer from 'material-ui/svg-icons/av/equalizer';
+const { showEnrolled } = require('../actions')
 
-const EnrolledButton = ({ dispatch }) =>
+
+const styles = {
+  smallIcon: {
+    width: 30,
+    height: 30,
+    color: 'dark-grey'
+  },
+  small: {
+    width: 72,
+    height: 72,
+    padding: 16,
+    margin: 40
+  },
+};
+
+
+exports.EnrolledButton = (dispatch) =>
   <span>
     <IconButton
       tooltip="Enrolled"
@@ -17,10 +34,25 @@ const EnrolledButton = ({ dispatch }) =>
     </IconButton>
   </span>
 
-const mapStateToProps = ({dispatch}) => {
-  return {
-    dispatch
-  }
-}
 
-module.exports = connect(mapStateToProps)(EnrolledButton)
+exports.ProgramButton = () =>
+<span>
+  <IconButton
+    tooltip="selected"
+    iconStyle={styles.smallIcon}
+    style={styles.small}
+  >
+    <Equalizer/>
+  </IconButton>
+</span>
+
+exports.AppliedButton = () =>
+  <span>
+    <IconButton
+      tooltip="Applied"
+      iconStyle={styles.smallIcon}
+      style={styles.small}
+    >
+      <Assignment/>
+    </IconButton>
+  </span>
